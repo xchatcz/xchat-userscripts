af18a29e14d98d37310a4b1f457ccb52ddacdafa
Jan Elznic <jan@elznic.com>
2025-09-05 10:38:55 +0200

    Skrytí vyskakovacího okna

---
+++ src/xchat-disable-room-popup.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         XChat - Místnosti - Skrytí vyskakovacího okna
+// @namespace    https://www.xchat.cz/
+// @version      1.0
+// @description  Skryje vyskakovací okno s potvrzením věku
+// @match        https://www.xchat.cz/~*~*/modchat*op=roomlist*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const popUp = document.querySelector('modalwin');
+  if (!popUp) return;
+  popUp.style.display = 'none';
+})();
